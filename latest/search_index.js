1e24892bf88b7c195eeb035cddfea699a08cd3eb
Documenter.jl <documenter@juliadocs.github.io>
2021-01-06 02:24:56 +0000

    build based on cb236b1

---
+++ latest/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api_reference/#API-Reference","page":"API Reference","title":"API Reference","text":"","category":"section"},{"location":"#CJKFrequencies.jl-Documentation","page":"Home","title":"CJKFrequencies.jl Documentation","text":"","category":"section"},{"location":"devdocs/#Developer-Docs","page":"Developer Docs","title":"Developer Docs","text":"","category":"section"},{"location":"devdocs/","page":"Developer Docs","title":"Developer Docs","text":"To add a new character frequency dataset","category":"page"}]
+}
